fa6c684222de2dc07698940c0036477f19f2365d
Carl <carl@carlossus.com>
2015-04-23 18:26:23 +0800

    Renamed 'init' to 'db'

--- src/store/init.js
+++
@@ -1,9 +0,0 @@
-var knex = require("knex");
-var path = require("path");
-var connection = knex({
-    client: "sqlite",
-    connection: {
-        filename: path.join(__dirname, "../southbeach.db")
-    }
-});
-module.exports = connection;
